5075f290a217680433c834be7153495632f1380b
Chinaza  Okeke <104084551+ChinazaOkeke@users.noreply.github.com>
2022-06-26 23:41:42 +0000

    i installed django the right way

--- chinazaokeke/chinazaokeke/asgi.py
+++ chinazaokeke/chinazaokeke/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for okekechinaza project.
+ASGI config for chinazaokeke project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'okekechinaza.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chinazaokeke.settings')
 
 application = get_asgi_application()
--- chinazaokeke/chinazaokeke/wsgi.py
+++ chinazaokeke/chinazaokeke/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for okekechinaza project.
+WSGI config for chinazaokeke project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'okekechinaza.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chinazaokeke.settings')
 
 application = get_wsgi_application()
